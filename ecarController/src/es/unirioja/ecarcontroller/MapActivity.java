9951c726b223c3cc4a6b2c75fec478cf82333fec
Rubén <rubenmoral7@gmail.com>
2013-02-18 17:30:57 +0100

    Creado mapa en MapActivity. Actualizado Manifest para mostrar el mapa.
    Signed-off-by: Rubén <rubenmoral7@gmail.com>

--- ecarController/src/es/unirioja/ecarcontroller/MapActivity.java
+++ ecarController/src/es/unirioja/ecarcontroller/MapActivity.java
@@ -1,22 +1,51 @@
 package es.unirioja.ecarcontroller;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
-public class MapActivity extends Activity {
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
+
+import android.location.Geocoder;
+import android.os.Bundle;
+
+public class MapActivity extends com.google.android.maps.MapActivity {
+	
+	private MapView map;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map);
+		initializeMap();
+	}
+	
+	private void initializeMap() {
+		map = (MapView) findViewById(R.id.map);
+		map.setBuiltInZoomControls(true);
+		map.getController().animateTo(searchLocationByName("Universidad de La Rioja, Logroño, España"));
+		map.getController().setZoom(18);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.map, menu);
-		return true;
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	private GeoPoint searchLocationByName(String locationName) {
+		Geocoder geoCoder = new Geocoder(this, Locale.getDefault());
+		GeoPoint gp = null;
+		try {
+			List<android.location.Address> addresses = geoCoder.getFromLocationName(locationName, 1);
+			for (android.location.Address address : addresses) {
+				gp = new GeoPoint((int)(address.getLatitude() * 1E6), (int)(address.getLongitude() * 1E6));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return gp;
 	}
 
 }
