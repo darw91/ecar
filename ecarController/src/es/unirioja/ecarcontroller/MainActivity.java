8a3b660ae621e5b2c7d201cfd15e16043716c991
ivan <ivanperez2@gmail.com>
2013-02-18 17:28:41 +0100

    Añadir dos botones a MainActivity.

--- ecarController/src/es/unirioja/ecarcontroller/MainActivity.java
+++ ecarController/src/es/unirioja/ecarcontroller/MainActivity.java
@@ -2,7 +2,11 @@ package es.unirioja.ecarcontroller;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button btMonitorizar = (Button) findViewById(R.id.btMonitorizar);
+        btMonitorizar.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(v.getContext(), MapActivity.class);
+				startActivity(i);
+			}
+		});
     }
 
     @Override
